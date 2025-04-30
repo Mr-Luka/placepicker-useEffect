1d79129949657dfb904110bad2931588e6e3a06b
Luka <luka.luka.luka93@gmail.com>
2025-04-30 13:03:21 -0700

    adding useEffect for the modal open and close after 3 seconds

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,7 @@
 import { useRef, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
-export default function Modal({open, children }) {
+export default function Modal({open, children, onClose }) {
   const dialog = useRef();
 
  useEffect(()=> {
@@ -13,8 +13,8 @@ export default function Modal({open, children }) {
  }, [open])
 
   return createPortal(
-    <dialog className="modal" ref={dialog} >
-      {children}
+    <dialog className="modal" ref={dialog} onClose={onClose}>
+      {open ? children : null}
     </dialog>,
     document.getElementById('modal')
   );
